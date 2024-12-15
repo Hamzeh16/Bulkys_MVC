7063df2e8945858666a3229a51141db1c2e53b64
Lana <Lana@DESKTOP-PSDT1OQ>
2024-12-15 17:21:51 +0300

    change

--- BulkyWeb/Areas/Student/Controllers/DrPageController.cs
+++ BulkyWeb/Areas/Student/Controllers/DrPageController.cs
@@ -31,5 +31,86 @@ namespace BulkyWeb.Areas.Student.Controllers
 
             return View(users);
         }
+        public async Task<IActionResult> cs()
+        {
+
+            var users = await _userManager.Users
+                    .Select(u => new ApplicationUser
+                    {
+                        Id = u.Id,
+                        Name = u.Name ?? "Unknown",           // Provide default value for null Name
+                        Majer = u.Majer ?? "Unknown",         // Provide default value for null Majer
+                        Rank = u.Rank ?? "Unranked",          // Provide default value for null Rank
+                        Office = u.Office ?? "Not Assigned",  // Provide default value for null Office
+                        ImageUrl = u.ImageUrl ?? "/images/default.png" // Provide default image URL for null ImageUrl
+                    }).ToListAsync();
+
+            return View(users);
+        }
+        public async Task<IActionResult> ai()
+        {
+
+            var users = await _userManager.Users
+                    .Select(u => new ApplicationUser
+                    {
+                        Id = u.Id,
+                        Name = u.Name ?? "Unknown",           // Provide default value for null Name
+                        Majer = u.Majer ?? "Unknown",         // Provide default value for null Majer
+                        Rank = u.Rank ?? "Unranked",          // Provide default value for null Rank
+                        Office = u.Office ?? "Not Assigned",  // Provide default value for null Office
+                        ImageUrl = u.ImageUrl ?? "/images/default.png" // Provide default image URL for null ImageUrl
+                    }).ToListAsync();
+
+            return View(users);
+        }
+        public async Task<IActionResult> bit()
+        {
+
+            var users = await _userManager.Users
+                    .Select(u => new ApplicationUser
+                    {
+                        Id = u.Id,
+                        Name = u.Name ?? "Unknown",           // Provide default value for null Name
+                        Majer = u.Majer ?? "Unknown",         // Provide default value for null Majer
+                        Rank = u.Rank ?? "Unranked",          // Provide default value for null Rank
+                        Office = u.Office ?? "Not Assigned",  // Provide default value for null Office
+                        ImageUrl = u.ImageUrl ?? "/images/default.png" // Provide default image URL for null ImageUrl
+                    }).ToListAsync();
+
+            return View(users);
+        }
+        public async Task<IActionResult> Cyber()
+        {
+
+            var users = await _userManager.Users
+                    .Select(u => new ApplicationUser
+                    {
+                        Id = u.Id,
+                        Name = u.Name ?? "Unknown",           // Provide default value for null Name
+                        Majer = u.Majer ?? "Unknown",         // Provide default value for null Majer
+                        Rank = u.Rank ?? "Unranked",          // Provide default value for null Rank
+                        Office = u.Office ?? "Not Assigned",  // Provide default value for null Office
+                        ImageUrl = u.ImageUrl ?? "/images/default.png" // Provide default image URL for null ImageUrl
+                    }).ToListAsync();
+
+            return View(users);
+        }
+        public async Task<IActionResult> software()
+        {
+
+            var users = await _userManager.Users
+                    .Select(u => new ApplicationUser
+                    {
+                        Id = u.Id,
+                        Name = u.Name ?? "Unknown",           // Provide default value for null Name
+                        Majer = u.Majer ?? "Unknown",         // Provide default value for null Majer
+                        Rank = u.Rank ?? "Unranked",          // Provide default value for null Rank
+                        Office = u.Office ?? "Not Assigned",  // Provide default value for null Office
+                        ImageUrl = u.ImageUrl ?? "/images/default.png" // Provide default image URL for null ImageUrl
+                    }).ToListAsync();
+
+            return View(users);
+        }
     }
+
 }
